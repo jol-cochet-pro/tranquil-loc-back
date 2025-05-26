907bbf06f1124379379982034e25bd5f5956500e
Jolan Cochet <jcochet@student.42angouleme.fr>
2025-05-26 16:39:45 +0200

    fix: make shared info dto with occupant and warrantor dto

--- src/shared-infos/dto/shared-infos.dto.ts
+++ src/shared-infos/dto/shared-infos.dto.ts
@@ -1,3 +1,11 @@
+import { userContactDtoSchema } from "src/users/dto/user-contact.dto";
 import { sharedInfosSchema } from "../entities/shared-infos.entity";
+import { occupantDtoSchema } from "src/occupants/dto/occupant.dto";
+import { warrantorDtoSchema } from "src/warrantors/dto/warrantor.dto";
+import { z } from "zod";
 
-export const sharedInfosDtoSchema = sharedInfosSchema;
+export const sharedInfosDtoSchema = sharedInfosSchema.extend({
+    user: userContactDtoSchema,
+    occupants: z.array(occupantDtoSchema),
+    warrantors: z.array(warrantorDtoSchema),
+});
